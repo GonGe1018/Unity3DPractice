953ab285a63c86b7d99a49df80a8669c9c7bc1da
GonGe1018 <chaeho1234@naver.com>
2023-04-06 23:59:48 +0900

    플레이어의 이동과 회전

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    new Camera camera;
+    Vector3 targetPos;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clickToRotate();
+
+    }
+
+    void clickToRotate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            // 마우스로 찍은 위치의 좌표 값을 가져온다
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000f))
+            {
+                if(hit.collider.name=="Floor")
+                {
+                    targetPos = hit.point;
+                    Player.transform.LookAt(targetPos);
+                }  
+            }
+        }
+    }
+}
---
+++ Assets/Script/PyrController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyrController : MonoBehaviour
+{
+    public Animator animator;
+
+    public new Camera camera;
+    
+    float hAxis; float vAxis; bool wDown;
+
+    float xRotate = 0.0f;
+    public float speed = 15f;
+    Vector3 moveVec;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+        
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");//A D
+        vAxis = Input.GetAxisRaw("Vertical");//W S
+        wDown = Input.GetButton("Walk");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis,0,vAxis).normalized;//호리즌과 버티컬 값을 vector3로 변환, 그리고 normolized를 통하여 벡터 값을 항상 1로 고정
+
+        transform.position += moveVec*speed*Time.deltaTime;
+
+        animator.SetBool("isRun",moveVec != Vector3.zero);//움직일 경우 isRun을 True로 바꿈
+        animator.SetBool("isWalk",wDown);
+
+        if(Input.GetMouseButton(1)==false)
+        {
+            transform.LookAt(transform.position + moveVec);
+        }
+        
+    }
+
+
+}
